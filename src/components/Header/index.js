7d319f6cd6005ca5183ef816a0e092ecb76a4fbe
Majaveed2002 <mohammedabduljaveed078@gmail.com>
2024-01-05 10:41:29 +0000

    added menubar functionality

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,35 @@
+import {useState} from 'react'
 import {HiMenu} from 'react-icons/hi'
+import {MdClose} from 'react-icons/md'
 import './index.css'
 
-const Header = () => (
-  <nav className="header-section">
-    <h1 className="user-name">Ma Javeed</h1>
-    <ul className="list-card">
-      <a href="#homeSection">About</a>
-      <a href="#projectSection">Projects</a>
-      <a href="#footerSection">Contact</a>
-    </ul>
-    <div className="icon-container">
-      <HiMenu size={30} />
-    </div>
-  </nav>
-)
+const Header = () => {
+  const [showMenu, toggleMenu] = useState(false)
+
+  const displayMenuItems = () => {
+    toggleMenu(!showMenu)
+  }
+  return (
+    <nav className="header-section">
+      <h1 className="user-name">Ma Javeed</h1>
+      <ul className="list-card">
+        <a href="#homeSection">About</a>
+        <a href="#projectSection">Projects</a>
+        <a href="#footerSection">Contact</a>
+      </ul>
+      <div className="icon-container">
+        {showMenu ? (
+          <div className="menu-items-container">
+            <MdClose size={30} onClick={displayMenuItems} />
+            <a href="#homeSection">About</a>
+            <a href="#projectSection">Projects</a>
+            <a href="#footerSection">Contact</a>
+          </div>
+        ) : (
+          <HiMenu onClick={displayMenuItems} size={30} />
+        )}
+      </div>
+    </nav>
+  )
+}
 export default Header
